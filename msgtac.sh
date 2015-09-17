4ca8b2f905f20b910f2287cfc9d1522ef53e727d
Mingye Wang <arthur200126@gmail.com>
2015-09-17 08:08:25 -0400

    msgtac: Remove bashism

--- msgtac.sh
+++ msgtac.sh
@@ -15,19 +15,19 @@ with \`!' and with messages < 100000."
 }
 
 # So bash gives us why there will be a problem automatically!
-if [ "$1" != '-' ]; then
+if [ "$1" && "$1" != '-' ]; then
 	exec 4<"$1"
 else
 	exec 4<&0 && [ "$2" ]
-fi || usage 
+fi || usage
 
 outpre="${2:-$1}"; outpre="${outpre%.po*}"
 
 IFS='' i=0
-while read -r -u 4 line; do case "$line" in
+while read -r line; do case "$line" in
 	('')	echo "
 #: !DUMMY:$((100000-i))"  >> "$outpre.rev.po"; i=$((i+1));;
 	(*) echo "$line" >> "$outpre.rev.po";;
-esac; done
+esac; done <&4
 
 msgcat -o "$outpre.rev.po" "$outpre.rev.po"
