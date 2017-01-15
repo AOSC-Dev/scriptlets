e5dd9ab904337387eb92248e81f6bb0427ed0567
Mingye Wang <arthur200126@gmail.com>
2017-01-15 13:07:14 -0500

    I guess this is enough?

--- pingus_font.py
+++ pingus_font.py
@@ -5,30 +5,34 @@ procedure in pingus.
 """
 import png
 
-palette = [(0xff, 0xff, 0xff, i) for i in range(1, 255)]
+palette = [(0xff, 0xff, 0xff, i) for i in range(255, -1, -1)]
 
-def ungamma(x: int) -> int:
-    """Terrible ungamma, [0, 256)"""
-    a = 0.055
-    srgb = x / 255.0
-    if srgb <= 0.04045:
-        return int(srgb / 12.92 * 255)
-    else:
-        return int(((srgb + a) / (1 + a)) ** 2.4 * 255)
-
-def read_pgm2(pgmf):
-    """A dirty P2 pgm reader for fontgen only."""
+def read_pgm2(pgmf) -> list:
+    """
+    A dirty P2 pgm reader for fontgen only.
+    
+    Actually returns List[list], but let's not mess that up for now.
+    """
     assert pgrm.readline() == 'P3\n'  # type
     assert pgrm.readline()[0] == '#'  # comment signature
     (w, h) = map(int, pgrm.readline().split())
     depth = int(pgmf.readline())
     assert depth <= 255
     
-    return (w, h, [list(itertools.islice(pgmf, w)) for i in range(h)])
+    return (w, h, [int(i) for i in pgmf])
 
 def convert(filename):
+    """
+    Takes a pgm and makes it a pingus png font sprite.
+    """
     global palette
     with open(filename) as pgmf:
         w, h, pixels = read_pgm2(pgmf)
     writer = png.Writer(size=(w,h), palette=palette, compression=9, bitdepth=8)
     
+    # We don't need any gamma here: freetype's greyscale output is a coverage map.
+    with open(filename[:-4] + '.png', 'wb'):
+        writer.write(pixels)
+
+import sys
+convert(sys.argv[1])
