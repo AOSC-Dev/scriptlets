d1b8f1a3b229fdd86fb7093b6cfe303d9b3205bb
liushuyu <liushuyu011@gmail.com>
2019-09-02 10:13:18 -0600

    make-jdk-tarball-new.sh: changes the provider ...
    ... of the prebuilt binaries

--- make-jdk-tarball-new.sh
+++ make-jdk-tarball-new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-JDK_VER='8u172-b03'
-JDK_BIN_VER='8u162-b12'
+JDK_VER='8u222-ga'
+ICEDTEA_WEB='1.8'
 
 function download_jdk_src() {
   wget http://hg.openjdk.java.net/jdk8u/jdk8u/archive/jdk${JDK_VER}.tar.gz \
@@ -35,10 +35,11 @@ EOF
 download_jdk_src
 mkdir binary; cd binary
 
-if ! which phantomjs > /dev/null 2>&1; then
-  LINKS=("http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-arm32-vfp-hflt.tar.gz"
-    "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-arm64-vfp-hflt.tar.gz"
-  "http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz")
+# disable automated extraction for now
+if ! which phantomjs-no > /dev/null 2>&1; then
+  LINKS=("https://cdn.azul.com/zulu-embedded/bin/zulu8.40.0.178-ca-jdk1.8.0_222-linux_aarch32hf.tar.gz"
+    "https://cdn.azul.com/zulu-embedded/bin/zulu8.40.0.178-ca-jdk1.8.0_222-linux_aarch64.tar.gz"
+  "https://cdn.azul.com/zulu/bin/zulu8.40.0.25-ca-jdk8.0.222-linux_x64.tar.gz")
   LINKS=${LINKS[*]}
   echo "PhantomJS not found, not able to dynamically extract links. Using default links to download binaries..."
 else
@@ -48,16 +49,21 @@ wget -c --no-cookies \
   --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
   ${LINKS}
 echo "Decompressing prebuilt binaries..."
-tar xf jdk*-linux-arm32-vfp-hflt.tar.gz
-mv jdk*/ armel/
-tar xf jdk*-linux-arm64-vfp-hflt.tar.gz
-mv jdk*/ arm64/
-tar xf jdk*-linux-x64.tar.gz
-mv jdk*/ amd64/
+tar xf zulu*-linux_aarch32hf.tar.gz
+mv zulu*/ armel/
+tar xf zulu*-linux_aarch64.tar.gz
+mv zulu*/ arm64/
+tar xf zulu*-linux_x64.tar.gz
+mv zulu*/ amd64/
 rm -rf -- *.tar.gz
+
 
 cd .. || exit 2
 mv binary openjdk-${JDK_VER}/
+
+wget -c "http://icedtea.wildebeest.org/download/source/icedtea-web-$ICEDTEA_WEB.tar.gz"
+tar xf "icedtea-web-$ICEDTEA_WEB.tar.gz"
+mv "icedtea-web-$ICEDTEA_WEB" openjdk-${JDK_VER}/icedtea-web
 
 if ! which pixz > /dev/null 2>&1; then
    echo "Compressing final tarball..."
