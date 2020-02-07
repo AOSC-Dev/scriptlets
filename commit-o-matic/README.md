af9191193dfdff71e5672cc3ddc568b95115c870
Leo Shen <szclsya@gmail.com>
2020-02-06 22:51:46 -0500

    commit-o-matic: new, a script to commit dull updates effectively

---
+++ commit-o-matic/README.md
@@ -0,0 +1,15 @@
+commit-o-matc
+-------------
+
+A simple script that commit and add new version message for you.
+
+```
+Useage:
+
+	commit-o-matic PACKAGE_GROUPS
+
+	- PACKAGE_GROUPS: Path to the list of packages. (Example: TREE/groups/plasma)
+
+```
+
+> No Buckling Spring Keyboards Were Harmed in the Making of This Script.
---
+++ commit-o-matic/commit-o-matic.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+_help_message() {
+	printf "\
+Useage:
+
+	commit-o-matic PACKAGE_GROUPS
+
+	- PACKAGE_GROUPS: Path to the list of packages. (Example: TREE/groups/plasma)
+
+"}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+	_help_message
+	exit 0
+fi
+
+if [ -z "$1" ]; then
+	echo -e "[!!!] Please specify a package group.\n"
+	_help_message
+	exit 1
+fi
+
+# Given a list of packages, automatically commit based on the new version number
+for i in $(cat $1); do
+    git add --all $i
+    git commit -m "${i##*/}: update to $(grep "VER=" $i/spec | cut -d "=" -f2)"
+done
+
