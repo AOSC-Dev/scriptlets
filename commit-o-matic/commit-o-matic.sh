59fb40efa5e5a1adb615fe4f64bcd48606c04f9d
Leo Shen <i@szclsya.me>
2020-03-24 05:15:10 +0000

    move bump-rel's git feature to commit-o-matic

--- commit-o-matic/commit-o-matic.sh
+++ commit-o-matic/commit-o-matic.sh
@@ -3,9 +3,10 @@ _help_message() {
 	printf "\
 Useage:
 
-	commit-o-matic PACKAGE_GROUPS
+	commit-o-matic PACKAGE_GROUP TYPE [MESSAGE]
 
-	- PACKAGE_GROUPS: Path to the list of packages. (Example: TREE/groups/plasma)
+	- PACKAGE_GROUP: Path to the list of packages to be committed.
+          (Example: TREE/groups/plasma)
 
 "
 }
@@ -21,8 +22,27 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+if [ -z "$2" ]; then
+	echo -e "[!!!] Please specify an operation.\n"
+	_help_message
+	exit 1
+fi
+
 # Given a list of packages, automatically commit based on the new version number
-for i in $(cat $1); do
-    git add --all $i
-    git commit -m "${i##*/}: update to $(grep "VER=" $i/spec | cut -d "=" -f2)"
-done
+if [[ $2 == "update" ]]; then
+	for i in $(cat $1); do
+    		git add --all $i
+    		git commit -m "${i##*/}: update to $(grep "VER=" $i/spec | cut -d "=" -f2)"
+	done
+elif [[ $2 == "bump-rel" ]]; then
+	if [ -z "$3" ]; then
+		echo -e "[!!!] Need a reason for revision."
+		_help_messgae
+		exit 1
+	fi
+
+	for i in $(cat $1); do
+		git add $i/spec
+		git commit -m "${i##*/}: $3"
+	done
+fi
