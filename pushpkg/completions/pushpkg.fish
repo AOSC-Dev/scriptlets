f5ffa52f1b5da561d71d45ff5e302ffc7bb2f3d9
liushuyu <liushuyu011@gmail.com>
2023-02-20 18:31:13 -0700

    pushpkg: add fish completion

---
+++ pushpkg/completions/pushpkg.fish
@@ -0,0 +1,16 @@
+function __pushpkg_complete_username
+    echo "$PUSHPKG_USERNAME" && whoami
+end
+
+function __pushpkg_complete_branch
+    string replace "OUTPUT-" "" (basename "$PWD")
+end
+
+complete -c pushpkg -s h -l help -d 'Print help information'
+complete -c pushpkg -s f -l force-push-noarch-package -d 'Force Push noarch package'
+complete -c pushpkg -s d -l delete -d 'Clean OUTPUT directory after finishing uploading'
+complete -c pushpkg -s r -l retro -d 'Push to AOSC OS/Retro repo'
+complete -c pushpkg -s v -l verbose -d 'Enable verbose logging for ssh and rsync'
+complete -xc pushpkg -n "__fish_is_nth_token 1" -a "(__pushpkg_complete_username)" -d 'Your LDAP username'
+complete -xc pushpkg -n "__fish_is_nth_token 2" -a "(__pushpkg_complete_branch)" -d 'AOSC OS update branch'
+complete -xc pushpkg -n "__fish_is_nth_token 3" -a "main bsp-sunxi bsp-rk bsp-rpi bsp-qcom" -d 'Repository component'
