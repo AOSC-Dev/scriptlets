1951f0a0f8b9d2ba669130479a136a004a3f40f6
xtex <xtexchooser@duck.com>
2025-01-26 15:15:44 +0800

    breakit: init
    This is just yet another PKGBREAK generator, providing better control
    over the package selecting process to user.
    
    This supports a PKGREBUILD syntax, for example:
    PKGREBUILD__ABI="sodep revdep \
    	path::usr/lib/python.*/.* \
    	path::.*\\.pyc \
    	section::lang-python"
    Will include:
    - All shared-library dependents.
    - All PKGDEP dependents.
    - All packages providing files under /usr/lib/pythonX.X
    - All packages under lang-python section.
    The third is important for upgrading Python versions.
    
    This also allows user to choose to bump REL of all broken packages but
    not committing, in order to avoid flashing the blinking firmware to their
    GPG keys.
    
    Another improvement is the ability to parse APML files more robustly.
    For example, breaker often generates PKGBREAK relationships like
    "XXX<=1:"${_UPSTREAM_VER}"", while BreakIt will print the evaluated
    version string in these cases.
    The two existing APML parsing libraries are both written in Rust and it
    seems to be a bit hard to invoke them in Python.
    
    Another feature is being capable to write the generated PKGBREAK back
    into defines files. With the power of LST, this will not break the
    existing code style of defines files.
    
    However, this is not an alternative to breaker, for that not all
    features in breaker are implemented here. I am not sure about what I
    missed but there must be something I missed. So use this tool at your
    own risk.

---
+++ breakit/src/pkgcontents.rs
@@ -0,0 +1,54 @@
+//! Helpers to scan the `Contents` file of a APT repository.
+
+use core::str;
+use std::collections::HashSet;
+
+use anyhow::Result;
+use bytes::Buf;
+use regex::Regex;
+
+pub(crate) async fn find_deps(
+	client: &reqwest::Client,
+	pattern: &Regex,
+) -> Result<HashSet<String>> {
+	let mut packages = HashSet::new();
+
+	for arch in ["all", "amd64", "arm64"] {
+		// TODO: cache Contents file
+		let resp = client
+			.execute(
+				client
+					.get(format!(
+						"https://repo.aosc.io/debs/dists/stable/main/Contents-{}.zst",
+						arch
+					))
+					.build()?,
+			)
+			.await?
+			.error_for_status()?
+			.bytes()
+			.await?;
+		let resp = zstd::decode_all(resp.reader())?;
+		let resp = String::from_utf8(resp)?;
+		for line in resp.lines() {
+			let line_bytes = line.as_bytes();
+			let mut idx = line_bytes.len() - 1;
+			while idx > 0 && line_bytes[idx] != b' ' {
+				idx -= 1;
+			}
+			if idx == 0 {
+				continue;
+			}
+			let path = str::from_utf8(&line_bytes[0..idx])?.trim_ascii_end();
+			if pattern.is_match(path) {
+				let pkg = str::from_utf8(&line_bytes[idx + 1..])?;
+				for pkg in pkg.split(',') {
+					let pkg = pkg.split('/').next_back().unwrap_or(pkg);
+					packages.insert(pkg.to_string());
+				}
+			}
+		}
+	}
+
+	Ok(packages)
+}
---
+++ breakit/src/pkgsite.rs
@@ -0,0 +1,62 @@
+//! AOSC OS packages site APIs.
+
+use std::collections::HashSet;
+
+use anyhow::Result;
+use serde::Deserialize;
+
+pub(crate) async fn find_deps(
+	client: &reqwest::Client,
+	package: &str,
+	sodep: bool,
+) -> Result<HashSet<String>> {
+	let resp = client
+		.execute(
+			client
+				.get(format!(
+					"https://packages.aosc.io/revdep/{}?type=json",
+					package
+				))
+				.build()?,
+		)
+		.await?
+		.error_for_status()?
+		.json::<PackageJson>()
+		.await?;
+	let mut packages = HashSet::new();
+
+	if !sodep {
+		for group in resp.revdeps {
+			for dep in group.deps {
+				packages.insert(dep.package);
+			}
+		}
+	} else {
+		for group in resp.sobreaks {
+			packages.extend(group);
+		}
+		packages.extend(resp.sobreaks_circular);
+	}
+
+	Ok(packages)
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageJson {
+	#[serde(default)]
+	revdeps: Vec<DependencyGroup>,
+	#[serde(default)]
+	sobreaks: Vec<Vec<String>>,
+	#[serde(default)]
+	sobreaks_circular: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct DependencyGroup {
+	deps: Vec<Dependency>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Dependency {
+	package: String,
+}
